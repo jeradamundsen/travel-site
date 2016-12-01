d358dc4d315df8dc3bbdfde4a3f2e315df20a2fa
theSoviet1984 <jerad.amundsen@gmail.com>
2016-11-30 18:12:47 -0700

    added icons and sprites

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -18,11 +18,17 @@ var config = {
 
 gulp.task('beginClean', function(){
   return del(['./app/temp/sprite', './app/assets/images/sprites']);
-})
+});
+
 gulp.task('createSprite',['beginClean'], function() {
   return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite/'));
+});
+
+gulp.task('copySpriteGraphic',['createSprite'], function(){
+  return gulp.src('./app/temp/sprite/css/**/*.svg')
+  .pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
 gulp.task('copySpriteCSS',['createSprite'], function(){
@@ -30,12 +36,8 @@ gulp.task('copySpriteCSS',['createSprite'], function(){
   .pipe(rename('_sprite.css'))
   .pipe(gulp.dest('./app/assets/styles/modules'))
 
-})
-gulp.task('copySpriteGraphic',['createSprite'], function(){
-  return gulp.src('./app/temp/sprite/css/**/*.svg')
-  .pipe(gulp.dest('./app/assets/images/sprites'));
-})
+});
 gulp.task('endClean',['copySpriteGraphic', 'copySpriteCSS'], function(){
   return del('./app/temp/sprite');
 })
-gulp.task('icons', ['beginClean','createSprite','copySpriteGraphic','copySpriteCSS']);
+gulp.task('icons', ['beginClean','createSprite','copySpriteGraphic','copySpriteCSS', 'endClean']);
