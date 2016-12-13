4cbbbdccc5020e093dce37fd517960c5430ed58c
theSoviet1984 <jerad.amundsen@gmail.com>
2016-12-13 13:31:54 -0600

    completed gulp build task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,10 +1,23 @@
 var gulp = require('gulp'),
 imagemin = require('gulp-imagemin'),
 del = require('del'),
-usemin = require('gulp-usemin');
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify'),
+browserSync = require('browser-sync').create();
 
-gulp.task('deleteDist', function(){
-  return gulp.task('./dist');
+gulp.task('previewDist', function(){
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "docs"
+    }
+  });
+})
+
+gulp.task('deleteDist',['icons'], function(){
+  return del('./docs');
 })
 gulp.task('optimizeImages',['deleteDist'], function(){
   return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
@@ -13,12 +26,17 @@ gulp.task('optimizeImages',['deleteDist'], function(){
     interlaced: true,
     multipass: true
   }))
-  .pipe(gulp.dest('./dist/assets/images'))
+  .pipe(gulp.dest('./docs/assets/images'))
 })
-
-gulp.task('usemin',['deleteDist'], function(){
+gulp.task('useminTrigger',['deleteDist'], function(){
+  gulp.start('usemin');
+})
+gulp.task('usemin',['styles','scripts'], function(){
   return gulp.src('./app/index.html')
-  .pipe(usemin())
-  .pipe(gulp.dest('./dist'))
+  .pipe(usemin({
+    css: [function(){return rev()},function(){return cssnano()}],
+    js: [function(){return rev()}, function(){return uglify()}]
+  }))
+  .pipe(gulp.dest('./docs'))
 })
-gulp.task('build',['deleteDist', 'optimizeImages','usemin'])
+gulp.task('build',['deleteDist', 'optimizeImages','usemin', 'useminTrigger'])
